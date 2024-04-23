18be882f9bcd99ea18eab8257e61efaaef23daec
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-04-23 08:58:45 +0800

    Reset Main:

--- Pertemuan 3/login_app/nomor2/SelfUtils.java
+++
@@ -1,8 +0,0 @@
-package nomor2;
-
-public class SelfUtils {
-          public static void displaySelfData() {
-                    System.out.println("Nama: Rohsilia Gratia Simak");
-                    System.out.println("NIM: H071231087");
-          }
-}
