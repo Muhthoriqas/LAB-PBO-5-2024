3b07c98bb1e5edee0e574671bc63a831c5611b40
AorusGit <arnoldjunior140202@gmail.com>
2024-06-11 21:41:40 +0800

    Add files via upload

---
+++ H071231044/P8/TP8_2_H071231044/No2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class No2 {
+    public static void main(String[] args) throws InterruptedException {
+        TypeRacer typeRacer = new TypeRacer();
+        typeRacer.setNewWordsToType();
+        System.out.println(" || Text to Type ||");
+        System.out.println("\"" + typeRacer.getWordsToType() + "\"");
+
+        Typer[] typers = new Typer[3];
+
+        typers[0] = new Typer("Bot Mansur", 80, typeRacer);
+        typers[1] = new Typer("Bot ToKu", 72, typeRacer);
+        typers[2] = new Typer("Bot Yukiao", 70, typeRacer);
+
+        typeRacer.getRaceContestant().addAll(Arrays.asList(typers));
+
+        typeRacer.startRace();
+        }
+    }
